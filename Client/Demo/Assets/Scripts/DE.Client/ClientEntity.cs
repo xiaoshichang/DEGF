9714738697b87283c85ee485ad1b33fc5c0884a3
xiaoshichang <511680777@qq.com>
2026-04-24 22:17:10 +0800

    update

--- Client/Demo/Assets/Scripts/DE.Client/ClientEntity.cs
+++ Client/Demo/Assets/Scripts/DE.Client/ClientEntity.cs
@@ -27,45 +27,5 @@ namespace DE.Client.Entities
         {
         }
     }
-
-    public sealed class ClientEntityManager
-    {
-        private readonly EntityDirectory<ClientEntity> mEntities = new EntityDirectory<ClientEntity>();
-
-        public int Count
-        {
-            get
-            {
-                return mEntities.Count;
-            }
-        }
-
-        public IEnumerable<ClientEntity> Entities
-        {
-            get
-            {
-                return mEntities.Entities;
-            }
-        }
-
-        public bool Add(ClientEntity entity)
-        {
-            return mEntities.Add(entity);
-        }
-
-        public bool Remove(Guid entityGuid)
-        {
-            return mEntities.Remove(entityGuid);
-        }
-
-        public bool TryGet(Guid entityGuid, out ClientEntity entity)
-        {
-            return mEntities.TryGet(entityGuid, out entity);
-        }
-
-        public void Clear()
-        {
-            mEntities.Clear();
-        }
-    }
+    
 }
